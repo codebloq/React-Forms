214ba8c3f33e038d0fa95f3ea3e2fe99113314d4
The Advocate <134937174+lawyerWho@users.noreply.github.com>
2023-05-31 18:05:44 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeading] = useState("");
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    // console.log(event.target.type);
+    // console.log(event.target.placeholder);
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setHeading(name);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        // Controlled component
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
